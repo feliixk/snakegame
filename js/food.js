f35b5b7254c7dece8fc28225b4f54bd50639c762
Felix <felixkivioja95@gmail.com>
2022-03-17 11:04:21 +0100

    added second food type Apple

--- js/food.js
+++ js/food.js
@@ -1,8 +1,11 @@
 import {onSnake, expandSnake} from './snake.js'
 import {randomGridPosition} from './grid.js'
 
+const foodType = ['food', 'foodApple']
+
 let score = 0
 let food = getRandomFoodPos()
+let selectedFoodType = selectFoodType()
 let EXPANSION_RATE = 1
 let SCORE_RATE = 1
 
@@ -10,7 +13,7 @@ export function update(){
     if (onSnake(food)){
         expandSnake(EXPANSION_RATE)
         food = getRandomFoodPos()
-
+        selectedFoodType = selectFoodType()
         score += SCORE_RATE
     }
 
@@ -22,10 +25,15 @@ export function draw(gameBoard){
     foodElement.style.gridRowStart = food.y
     foodElement.style.gridColumnStart = food.x
 
+    if (selectedFoodType == 'food'){
         foodElement.classList.add('food')
         EXPANSION_RATE = 1
         SCORE_RATE = 1
-    
+    }else{
+        foodElement.classList.add('foodApple')
+        EXPANSION_RATE = 3
+        SCORE_RATE = 3
+    }
     gameBoard.appendChild(foodElement)
 }
 
@@ -35,6 +43,19 @@ function getRandomFoodPos(){
         newFoodPosition = randomGridPosition()
     }
     return newFoodPosition
+}
+
+function selectFoodType(){
+
+    let number = Math.random() * 100
+    let selectedFood
+
+    if (number < 85){
+        selectedFood = foodType[0]
+    }else{
+        selectedFood = foodType[1]
+    }
+    return selectedFood
 }
 
 export function getScore(){
